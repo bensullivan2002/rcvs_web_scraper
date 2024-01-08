8cbe4591db2c923e02339502da8ffad7c6d75dc5
Ben Sullivan <bensullivan2002@gmail.com>
2024-01-08 12:45:40 +0000

    Installing dependencies.

---
+++ src/rcvs_web_scraper.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_url():
+    return input("Enter the url of the website: ")
---
+++ test/test_rcvs_web_scraper.py
@@ -0,0 +1,6 @@
+import pytest
+import hypothesis
+
+
+def test_get_url():
+    pass
