9fd4a5ed070200ba9e0593e0d718d66b2ef2fc3d
Ben Sullivan <bensullivan2002@gmail.com>
2024-01-20 09:50:03 +0000

    Identify practice names and addresses and add to list. Previous commit actually identified practice names, not addresses.

--- src/rcvs_web_scraper.py
+++ src/rcvs_web_scraper.py
@@ -20,8 +20,15 @@ these_pages = range(1, 3)
 for this_page in these_pages:
     html_data = get_url(base_url, this_page)
     soup = make_soup(html_data)
-    all_practices = soup.select("div.practice h2.item-title")
+
+    all_practice_names = soup.select("div.practice h2.item-title")
+    practice_names = []
+    for practice in all_practice_names:
+        practice_names.append((practice.get_text(strip=True)))
+    print(practice_names)
+
+    all_practice_addresses = soup.select("div.practice div.item-address")
     practice_addresses = []
-    for practice in all_practices:
+    for practice in all_practice_addresses:
         practice_addresses.append((practice.get_text(strip=True)))
     print(practice_addresses)
