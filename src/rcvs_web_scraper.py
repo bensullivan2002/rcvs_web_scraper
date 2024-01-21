f432c8513f225cb06ec9ab7190ee0029683766f4
Ben Sullivan <bensullivan2002@gmail.com>
2024-01-21 08:10:04 +0000

    Producing Pandas dataframe but not handling missing fields.

--- src/rcvs_web_scraper.py
+++ src/rcvs_web_scraper.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+import pandas as pd
 
 base_url = """
 https://findavet.rcvs.org.uk/find-a-vet-practice/?filter-choice=&filter-keyword=+&filter-searchtype=practice&p="""
@@ -31,4 +33,23 @@ for this_page in these_pages:
     practice_addresses = []
     for practice in all_practice_addresses:
         practice_addresses.append((practice.get_text(strip=True)))
-    print(practice_addresses)
+
+    all_practice_tels = soup.select("div.practice div.item-contact span.item-contact-tel")
+    temp_practice_tels = []
+    for practice in all_practice_tels:
+        temp_practice_tels.append((practice.get_text(strip=True)))
+    practice_tels = []
+    for temp_practice_tel in temp_practice_tels:
+        practice_tels.append(re.sub(r"phone2", "", temp_practice_tel))
+
+    all_practice_emails = soup.select("div.practice div.item-contact a.item-contact-email")
+    temp_practice_emails = []
+    for practice in all_practice_emails:
+        temp_practice_emails.append((practice.get_text(strip=True)))
+    practice_emails = []
+    for temp_practice_email in temp_practice_emails:
+        practice_emails.append(re.sub(r"envelope", "", temp_practice_email))
+
+    df = pd.DataFrame(list(zip(practice_names, practice_addresses, practice_tels, practice_emails)),
+                      columns=["Name", "Address", "Tel", "Email"])
+    print(df)
