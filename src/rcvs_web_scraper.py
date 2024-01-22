7b304edca7109f6ef1c4bb92a78c86b66a30537e
Ben Sullivan <bensullivan2002@gmail.com>
2024-01-22 10:41:24 +0000

    separated out postcodes and fixed some bugs.

--- src/rcvs_web_scraper.py
+++ src/rcvs_web_scraper.py
@@ -21,6 +21,7 @@ these_pages = range(1, 544)
 
 practice_names = []
 practice_addresses = []
+practice_postcodes = []
 practice_tels = []
 practice_emails = []
 
@@ -33,13 +34,16 @@ for this_page in these_pages:
     for practice in practices:
 
         practice_names.append(practice.h2.text.strip())
+
         practice_addresses.append(practice.div.text.strip())
+
         contacts = practice.find("div", class_="item-contact")
         try:
             temp_phone = contacts.span.text.strip()
         except Exception as e:
             temp_phone = ""
         stripped_temp_phone = re.sub("\n\n\n ", "", temp_phone)
+
         practice_tels.append(re.sub("phone2", "", stripped_temp_phone))
         try:
             temp_email = contacts.a.text.strip()
@@ -48,8 +52,21 @@ for this_page in these_pages:
         stripped_temp_email = re.sub("\n\n\n ", "", temp_email)
         practice_emails.append(re.sub("envelope", "", stripped_temp_email))
 
-df = pd.DataFrame(list(zip(practice_names, practice_addresses, practice_tels, practice_emails)),
-                  columns=["Name", "Address", "Tel", "Email"])
-print(df)
+for practice_address in practice_addresses:
+    results = re.findall("^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$", practice_address)
+    count = 0
+    if len(results) == 0:
+        postcode = ""
+        practice_postcodes.append(postcode)
+    if len(results) > 0:
+        for result in results[0]:
+            if len(result) > count:
+                count = len(result)
+                postcode = result
+                practice_postcodes.append(postcode)
+
+df = pd.DataFrame(list(zip(practice_names, practice_addresses, practice_postcodes, practice_tels, practice_emails)),
+                  columns=["Name", "Address", "Postcode", "Tel", "Email"])
+# print(df)
 
 df.to_csv("out.csv", index=False)
