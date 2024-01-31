c8ef984419de13bbbfe6a3558270d48a33992778
Ben Sullivan <bensullivan2002@gmail.com>
2024-01-31 10:27:54 +0000

    Trying to get Read the Docs working.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ release = '1.0'
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../src.main"))
+sys.path.insert(0, os.path.abspath("../src/main"))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
