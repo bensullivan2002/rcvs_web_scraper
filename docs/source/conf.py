78ea991a1fd960d3ecb2034223c2ec6b4aca18fa
Ben Sullivan <bensullivan2002@gmail.com>
2024-02-02 13:30:52 +0000

    Adding various configs for Read the Docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,8 @@
+import pathlib
+import sys
+
+sys.path.insert(0, str(pathlib.Path(__file__).parents[2].resolve().as_posix()))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -9,26 +14,27 @@
 project = 'RCVS Web Scraper'
 copyright = '2024, Ben Sullivan'
 author = 'Ben Sullivan'
-release = '1.0'
-
-# -- Path setup --------------------------------------------------------------
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath("../src/main"))
+release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.napoleon']
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'nbsphinx',
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'English'
+language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
