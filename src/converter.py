24f04862f59007397cc48e9edb92b5058586846a
bensullivan2002 <bensullivan2002@gmail.com>
2024-02-25 08:45:54 +0000

    r

--- src/converter.py
+++ src/converter.py
@@ -2,5 +2,5 @@ from json import loads
 
 
 def dataframe_to_json(dataframe):
-    result = dataframe.to_json(orient="split")
+    result = dataframe.to_json('rcvs.json', orient="split")
     return loads(result)
