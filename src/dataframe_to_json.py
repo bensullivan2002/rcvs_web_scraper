2a8406d05fdf89da9b7d53bacb1d7b411ee7b445
bensullivan2002 <bensullivan2002@gmail.com>
2024-02-25 08:09:54 +0000

    r

--- src/dataframe_to_json.py
+++ src/dataframe_to_json.py
@@ -1,6 +1,8 @@
 from json import loads, dumps
 
-df = src.rcvs_web_scraper.df
+from src import rcvs_web_scraper
+
+df = rcvs_web_scraper.df
 
 result = df.to_json(orient="split")
 parsed = loads(result)
